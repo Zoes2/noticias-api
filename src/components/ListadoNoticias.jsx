956575cd90a07f948df0c492368d0a7ef0843b03
Zoes2 <v.meneses20.20@gmail.com>
2023-11-28 22:26:08 -0300

    creamos un paginador para las noticias

--- src/components/ListadoNoticias.jsx
+++ src/components/ListadoNoticias.jsx
@@ -1,6 +1,8 @@
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import useNoticias from '../hooks/useNoticias'
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 import Noticia from './Noticia'
 
 const ListadoNoticias = () => {
@@ -30,6 +32,21 @@ const ListadoNoticias = () => {
                     />
                 ))}
             </Grid>
+
+            <Stack 
+                sx={{
+                    marginY: 5
+                }}
+                spacing={2}
+                direction={"row"}
+                justifyContent={"center"}
+                alignItems={"center"}
+            >
+                <Pagination 
+                    count={10} 
+                    color="primary" 
+                />
+            </Stack>
         </>
     )
 }
